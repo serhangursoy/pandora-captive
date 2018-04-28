21132330f93ea655dd2e953cffd09f2584382a95
Serhan Gürsoy <gursoyserhan@yandex.com>
2018-04-29 00:53:39 +0300

    Changes
    Optimized changes for the box

--- install.sh
+++ install.sh
@@ -5,31 +5,33 @@ if [ "$(id -u)" != "0" ]; then
    exit 1
 fi
 
+echo "Updating.. "
+
 sudo apt-get update
+echo "Update completed.."
 
 echo "Installing dependencies..."
 
 apt-get install macchanger
 apt-get install hostapd
 apt-get install dnsmasq
-apt-get install apache2
-apt-get install php
+# apt-get install apache2
+# apt-get install php
 
-
-echo "Skipping NodeJS for now Egiş"
+# echo "Skipping NodeJS for now Egiş"
 
 echo "Configuring components..."
 cp -f hostapd.conf /etc/hostapd/
 cp -f dnsmasq.conf /etc/
-cp -Rf html /var/www/
-chown -R www-data:www-data /var/www/html
-chown root:www-data /var/www/html/.htaccess
+# cp -Rf html /var/www/
+# chown -R www-data:www-data /var/www/html
+# chown root:www-data /var/www/html/.htaccess
 cp -f rc.local /etc/
-cp -f override.conf /etc/apache2/conf-available/
-cd /etc/apache2/conf-enabled
-ln -s ../conf-available/override.conf override.conf
-cd /etc/apache2/mods-enabled
-ln -s ../mods-available/rewrite.load rewrite.load
+# cp -f override.conf /etc/apache2/conf-available/
+# cd /etc/apache2/conf-enabled
+# ln -s ../conf-available/override.conf override.conf
+# cd /etc/apache2/mods-enabled
+# ln -s ../mods-available/rewrite.load rewrite.load
 
-echo "Rogue captive portal installed. Reboot to execute."
+echo "Captive portal installed. Reboot to execute. Make sure that you are running your game at 127.0.0.1:8081 and backend at 127.0.0.1:8080!"
 exit 0
